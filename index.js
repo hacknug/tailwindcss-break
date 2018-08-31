b23fe3bed23b7ee453a428bb0971bb2038fd1f50
Nestor Vera <nestorvera@me.com>
2018-08-31 14:12:03 +0200

    Init repository

---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = function (variants) {
+  return function ({ addUtilities }) {
+    addUtilities(
+      {
+        // Break Before: Generic Values
+        '.bb-auto': { breakBefore: 'auto' },
+        '.bb-avoid': { breakBefore: 'avoid' },
+
+        // Break Before: Page Values
+        '.bb-avoid-page': { breakBefore: 'avoid-page' },
+        '.bb-page': { breakBefore: 'page' },
+        '.bb-always': { breakBefore: 'always' },
+        '.bb-left': { breakBefore: 'left' },
+        '.bb-right': { breakBefore: 'right' },
+        '.bb-recto': { breakBefore: 'recto' },
+        '.bb-verso': { breakBefore: 'verso' },
+
+        // Break Before: Column Values
+        '.bb-avoid-column': { breakBefore: 'avoid-column' },
+        '.bb-column': { breakBefore: 'column' },
+
+        // Break Before: Region Values
+        '.bb-avoid-region': { breakBefore: 'avoid-region' },
+        '.bb-region': { breakBefore: 'region' },
+
+        // Break After: Generic Values
+        '.ba-auto': { breakAfter: 'auto' },
+        '.ba-avoid': { breakAfter: 'avoid' },
+
+        // Break After: Page Values
+        '.ba-avoid-page': { breakAfter: 'avoid-page' },
+        '.ba-page': { breakAfter: 'page' },
+        '.ba-always': { breakAfter: 'always' },
+        '.ba-left': { breakAfter: 'left' },
+        '.ba-right': { breakAfter: 'right' },
+        '.ba-recto': { breakAfter: 'recto' },
+        '.ba-verso': { breakAfter: 'verso' },
+
+        // Break After: Column Values
+        '.ba-avoid-column': { breakAfter: 'avoid-column' },
+        '.ba-column': { breakAfter: 'column' },
+
+        // Break After: Region Values
+        '.ba-avoid-region': { breakAfter: 'avoid-region' },
+        '.ba-region': { breakAfter: 'region' },
+
+        // Break Inside
+        '.bi-auto': { breakInside: 'auto' },
+        '.bi-avoid': { breakInside: 'avoid' },
+        '.bi-avoid-page': { breakInside: 'avoid-page' },
+        '.bi-avoid-column': { breakInside: 'avoid-column' },
+        '.bi-avoid-region': { breakInside: 'avoid-region' },
+      },
+      variants
+    )
+  }
+}
---
+++ readme.md
@@ -0,0 +1,60 @@
+# Tailwind CSS Break Plugin
+
+This plugin adds utilities to use break with Tailwind CSS.
+
+## Installation
+
+Add this plugin to your project:
+
+```bash
+# Install using pnpm
+pnpm install --save-dev tailwindcss-break
+
+# Install using npm
+npm install --save-dev tailwindcss-break
+
+# Install using yarn
+yarn add -D tailwindcss-break
+```
+
+## Usage
+
+```js
+require('tailwindcss-break')(['responsive'])
+```
+
+```css
+.bb-auto { break-before: auto; }
+.bb-avoid { break-before: avoid; }
+.bb-avoid-page { break-before: avoid-page; }
+.bb-page { break-before: page; }
+.bb-always { break-before: always; }
+.bb-left { break-before: left; }
+.bb-right { break-before: right; }
+.bb-recto { break-before: recto; }
+.bb-verso { break-before: verso; }
+.bb-avoid-column { break-before: avoid-column; }
+.bb-column { break-before: column; }
+.bb-avoid-region { break-before: avoid-region; }
+.bb-region { break-before: region; }
+
+.ba-auto { break-after: auto; }
+.ba-avoid { break-after: avoid; }
+.ba-avoid-page { break-after: avoid-page; }
+.ba-page { break-after: page; }
+.ba-always { break-after: always; }
+.ba-left { break-after: left; }
+.ba-right { break-after: right; }
+.ba-recto { break-after: recto; }
+.ba-verso { break-after: verso; }
+.ba-avoid-column { break-after: avoid-column; }
+.ba-column { break-after: column; }
+.ba-avoid-region { break-after: avoid-region; }
+.ba-region { break-after: region; }
+
+.bi-auto { break-inside: auto; }
+.bi-avoid { break-inside: avoid; }
+.bi-avoid-page { break-inside: avoid-page; }
+.bi-avoid-column { break-inside: avoid-column; }
+.bi-avoid-region { break-inside: avoid-region; }
+```
